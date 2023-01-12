4072fd2fea1ffc5baae1cbf45b7edf11ec34e733
adip28 <adi.permana60@ymail.com>
2023-01-12 19:12:20 +0700

    Fragmentorder

--- app/src/main/java/com/example/blits/customer/FragmentOrder.java
+++ app/src/main/java/com/example/blits/customer/FragmentOrder.java
@@ -1,30 +1,22 @@
 package com.example.blits.customer;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.viewpager.widget.PagerAdapter;
-import androidx.viewpager.widget.ViewPager;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageButton;
 
-import com.android.volley.RequestQueue;
-import com.example.blits.AdapterTabPesanan;
-import com.example.blits.FragmentProgress;
-import com.example.blits.FragmentSuccess;
 import com.example.blits.R;
-import com.example.blits.adapter.AdapterDashboardDriver;
 import com.example.blits.adapter.AdapterPemesanan;
-import com.example.blits.model.DriverModel;
 import com.example.blits.model.ModelUser;
 import com.example.blits.model.PesananModel;
 import com.example.blits.network.NetworkService;
@@ -35,7 +27,6 @@ import com.example.blits.service.App;
 import com.example.blits.service.GsonHelper;
 import com.example.blits.service.Prefs;
 import com.example.blits.ui.SweetDialogs;
-import com.google.android.material.tabs.TabLayout;
 import com.google.gson.Gson;
 import com.ontbee.legacyforks.cn.pedant.SweetAlert.SweetAlertDialog;
 
@@ -45,25 +36,113 @@ import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 
-public class FragmentOrder extends Fragment {
+public class FragmentOrder extends Fragment implements AdapterPemesanan.onSelected {
 
-    private TabLayout tabLayout;
-    private ViewPager viewPager;
+    RecyclerView mRecyclerView;
+    RecyclerView.Adapter adapter;
+
+    List<ModelUser> modelDashboard;
+    ModelUser modelUser;
+    Dialog dialog;
+    ImageButton closePopup;
+    SweetAlertDialog sweetAlertDialog;
+    public final Retrofit restService = RestService.getRetrofitInstance();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_order, container, false);
+        View v = inflater.inflate(R.layout.fragment_progress, container, false);
+        dialog = new Dialog(getActivity());
+        sweetAlertDialog = new SweetAlertDialog(getActivity());
+        mRecyclerView = v.findViewById(R.id.mRecyclerView);
 
-        tabLayout = v.findViewById(R.id.tabLayout);
-        viewPager = v.findViewById(R.id.viewPager);
+        modelUser = (ModelUser) GsonHelper.parseGson(App.getPref().getString(Prefs.PREF_STORE_PROFILE, ""), new ModelUser());
 
-        tabLayout.setupWithViewPager(viewPager);
-
-        AdapterTabPesanan adapterTabPesanan = new AdapterTabPesanan(getActivity().getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
-        adapterTabPesanan.addFragment(new FragmentProgress(), "PESANAN");
-        adapterTabPesanan.addFragment(new FragmentSuccess(), "RIWAYAT");
-        viewPager.setAdapter(adapterTabPesanan);
-
+        ListPesanan();
         return v;
+    }
+
+    private void ListPesanan() {
+        showLoadingIndicator();
+        restService.create(NetworkService.class).getPesanan(modelUser.getGuid())
+                .enqueue(new Callback<PesananResponse>() {
+                    @Override
+                    public void onResponse(retrofit2.Call<PesananResponse> call, Response<PesananResponse> response) {
+                        hideLoadingIndicator();
+                        Log.d("datanyainiwoi" , new Gson().toJson(response.body()));
+                        if(response.body().getmStatus())
+                            onDataReady(response.body().getData());
+                        else
+                            SweetDialogs.commonInvalidToken(getActivity() ,"Gagal Memuat Permintaan" ,response.body().getmRm());
+                    }
+
+                    @Override
+                    public void onFailure(retrofit2.Call<PesananResponse> call, Throwable t) {
+                        hideLoadingIndicator();
+                        onNetworkError(t.getLocalizedMessage());
+                    }
+                });
+    }
+
+    private void getDriver(String guid_driver) {
+        Log.d("guid drivery" , guid_driver);
+        showLoadingIndicator();
+        restService.create(NetworkService.class).getDriverByGuid(guid_driver)
+                .enqueue(new Callback<DriverResponse>() {
+                    @Override
+                    public void onResponse(retrofit2.Call<DriverResponse> call, Response<DriverResponse> response) {
+                        hideLoadingIndicator();
+                        showDetailDriver();
+//                        if(response.body().getmStatus())
+//                            onDataReady(response.body().getData());
+//                        else
+//                            SweetDialogs.commonInvalidToken(getActivity() ,"Gagal Memuat Permintaan" ,response.body().getmRm());
+                    }
+
+                    @Override
+                    public void onFailure(retrofit2.Call<DriverResponse> call, Throwable t) {
+                        hideLoadingIndicator();
+                        onNetworkError(t.getLocalizedMessage());
+                    }
+                });
+    }
+
+    private void showDetailDriver() {
+        dialog.setContentView(R.layout.popup_detaildriver);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
+        dialog.setCanceledOnTouchOutside(false);
+        closePopup = dialog.findViewById(R.id.closePopKeuntungan);
+        closePopup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+    }
+
+    void onDataReady(List<PesananModel> model){
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
+        mRecyclerView.clearFocus();
+        adapter = new AdapterPemesanan(getActivity(), model, FragmentOrder.this);
+        mRecyclerView.setAdapter(adapter);
+    }
+
+    public void showLoadingIndicator() {
+        sweetAlertDialog.show();
+    }
+
+    public void hideLoadingIndicator() {
+        sweetAlertDialog.dismiss();
+    }
+
+    public void onNetworkError(String cause) {
+        Log.d("Error", cause);
+        SweetDialogs.endpointError(getActivity());
+    }
+
+    @Override
+    public void onDetailDriver(PesananModel data) {
+        getDriver(data.getGuid_driver());
     }
 }
